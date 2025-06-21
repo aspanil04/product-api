960ba1ba3fe4dafe02e573b83ec30f2c6d1e4921
aspanil04 <asp.anil04@gmail.com>
2025-06-21 23:13:46 +0530

    kubernetes file update

--- obj/Debug/net7.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e1298b4fe1aeddac05df3e4e85f18961b3d9fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1116525ff11ddfb903afc422d426d40c4cbc948")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
